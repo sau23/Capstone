e9bfb668c3a44d52a7c309fcc054b08d58d5692b
sam <sau23@scarletmail.rutgers.edu>
2019-01-29 12:32:00 -0500

    Add login, integrate MySQL

---
+++ app/controllers/login_controller.rb
@@ -0,0 +1,4 @@
+class LoginController < ApplicationController
+  def homepage
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'login/homepage'
+  root 'login#homepage'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
