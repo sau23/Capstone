5caca67c9632c8fee224d65ca52bfabbe0f5c62b
sam <sau23@scarletmail.rutgers.edu>
2019-03-05 13:56:54 -0500

    Add jquery

--- Gemfile
+++ Gemfile
@@ -20,6 +20,7 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
 gem 'coffee-script-source', '1.8.0'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
+gem 'jquery-rails'
 gem 'turbolinks', '~> 5'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.5'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require chartkick
+//= require jquery
+//= require jquery_ujs
 //= require bootstrap-sprockets
+//= require chartkick
 //= require rails-ujs
 //= require activestorage
-= require turbolinks
+//= require turbolinks
 //= require_tree .
